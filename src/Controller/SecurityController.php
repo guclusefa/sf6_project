88aed47e96dbc3f526710c12f77002d8795b7c4e
Sefa GUCLU <sefaaguclu@gmail.com>
2022-11-13 15:50:36 +0100

    register avec mail confirmation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,12 +12,15 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            // error if user is already logged in
+            $this->addFlash('error', 'Vous êtes déjà connecté');
+            return $this->redirectToRoute('personne_all');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
